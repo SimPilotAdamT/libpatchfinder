89ee96944ab02ddffe13df4a0ad42788348dd727
Adam Tazul <71192298+SimPilotAdamT@users.noreply.github.com>
2024-03-04 22:11:35 +0000

    Update OFexception.hpp

--- include/libpatchfinder/OFexception.hpp
+++ include/libpatchfinder/OFexception.hpp
@@ -15,14 +15,15 @@ namespace tihmstar {
         ExceptionData _exceptionData;
         public:
             // Inheriting constructors from the base class
-            //using exception::exception;
-            OFexception(const char* commit_count_str, const char* commit_sha_str, int line, const char* filename)
-                : _exceptionData{commit_count_str, commit_sha_str, line, filename}, exception(_exceptionData) {}
-            void initializeBaseClass() {
-                exception::commit_count_str = _commit_count_str;
-                exception::commit_sha_str = _commit_sha_str;
-                exception::line = _line;
-                exception::filename = _filename;
+            using exception::exception;
+            explicit OFexception(const char* message) : exception(message) {}
+            //OFexception(const char* commit_count_str, const char* commit_sha_str, int line, const char* filename)
+              //  : _exceptionData{commit_count_str, commit_sha_str, line, filename}, exception(_exceptionData) {}
+            //void initializeBaseClass() {
+              //  exception::commit_count_str = _commit_count_str;
+                //exception::commit_sha_str = _commit_sha_str;
+                //exception::line = _line;
+                //exception::filename = _filename;
             }
     };
 
