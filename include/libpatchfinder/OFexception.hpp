0440d3ac04e242977e653ea697fa6f116825362e
Adam Tazul <71192298+SimPilotAdamT@users.noreply.github.com>
2024-03-04 21:43:27 +0000

    Update OFexception.hpp

--- include/libpatchfinder/OFexception.hpp
+++ include/libpatchfinder/OFexception.hpp
@@ -1,11 +1,3 @@
-//
-//  OFexception.h
-//  libpatchfinder
-//
-//  Created by tihmstar on 24.02.19.
-//  Copyright © 2019 tihmstar. All rights reserved.
-//
-
 #ifndef OFexception_hpp
 #define OFexception_hpp
 
@@ -13,48 +5,47 @@
 #include <libgeneral/exception.hpp>
 
 namespace tihmstar {
-    class OFexception : public tihmstar::exception{
+    class OFexception : public tihmstar::exception {
+    public:
+        // Inheriting constructors from the base class
         using exception::exception;
     };
-    //custom exceptions for makeing it easy to catch
-    class out_of_range : public OFexception{
+
+    // Custom exceptions for making it easy to catch
+    class out_of_range : public OFexception {
         using OFexception::OFexception;
     };
 
-    class not_found : public OFexception{
+    class not_found : public OFexception {
         using OFexception::OFexception;
     };
 
-    
-    class symbol_not_found : public OFexception{
+    class symbol_not_found : public OFexception {
         using OFexception::OFexception;
     };
 
     class load_command_not_found : public OFexception {
         int _cmd;
 
-        public:
-            int cmd() const { return _cmd; };
+    public:
+        int cmd() const { return _cmd; };
 
-            // Adjusted constructor to avoid passing arguments to ellipsis
-            load_command_not_found(const char *commit_count_str, const char *commit_sha_str, int line, const char *filename, int cmd)
-                : OFexception(commit_count_str, commit_sha_str, line, filename), _cmd(cmd) {}
-
+        // Constructor for load_command_not_found
+        load_command_not_found(const char *commit_count_str, const char *commit_sha_str, int line, const char *filename, int cmd)
+            : OFexception(commit_count_str, commit_sha_str, line, filename), _cmd(cmd) {}
     };
 
-    
-    class symtab_not_found : public OFexception{
+    class symtab_not_found : public OFexception {
         using OFexception::OFexception;
     };
-    
-    class limit_reached : public OFexception{
-        using OFexception::OFexception;
-    };
-    
-    class bad_branch_destination : public OFexception{
-        using OFexception::OFexception;
-    };
-};
 
+    class limit_reached : public OFexception {
+        using OFexception::OFexception;
+    };
+
+    class bad_branch_destination : public OFexception {
+        using OFexception::OFexception;
+    };
+}
 
 #endif /* OFexception_hpp */
