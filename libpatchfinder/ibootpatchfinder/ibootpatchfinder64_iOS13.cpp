741c2530df56cd68723761818cad00138a6cb5dc
Adam Tazul <71192298+SimPilotAdamT@users.noreply.github.com>
2024-03-04 21:08:15 +0000

    Update ibootpatchfinder64_iOS13.cpp

--- libpatchfinder/ibootpatchfinder/ibootpatchfinder64_iOS13.cpp
+++ libpatchfinder/ibootpatchfinder/ibootpatchfinder64_iOS13.cpp
@@ -190,6 +190,13 @@ uint32_t ibootpatchfinder64_iOS13::get_el1_pagesize(){
     debug("tcr_el1_val=0x%016llx",tcr_el1_val);
 
     uint8_t TG0 = (tcr_el1_val >> 14) & 0b11;
+
+    #define reterror(format, ...) \
+    do { \
+        char errmsg[256]; \
+        snprintf(errmsg, sizeof(errmsg), format, ##__VA_ARGS__); \
+        throw tihmstar::patchfinder::load_command_not_found(__FILE__, __func__, __LINE__, errmsg, TG0); \
+    } while(0)
     
     switch (TG0) {
         case 0b00:
