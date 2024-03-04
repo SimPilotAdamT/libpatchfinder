b092ddf85a6c70b6665673f9c2ddcec092906121
Adam Tazul <71192298+SimPilotAdamT@users.noreply.github.com>
2024-03-04 21:44:45 +0000

    Update OFexception.hpp

--- include/libpatchfinder/OFexception.hpp
+++ include/libpatchfinder/OFexception.hpp
@@ -31,7 +31,7 @@ namespace tihmstar {
         int cmd() const { return _cmd; };
 
         // Constructor for load_command_not_found
-        load_command_not_found(const char *commit_count_str, const char *commit_sha_str, int line, const char *filename, int cmd)
+        load_command_not_found(const char* commit_count_str, const char* commit_sha_str, int line, const char* filename, int cmd)
             : OFexception(commit_count_str, commit_sha_str, line, filename), _cmd(cmd) {}
     };
 
