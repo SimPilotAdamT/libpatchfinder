e2497bd0a308c3382f2397dc263742140dc70d36
Adam Tazul <71192298+SimPilotAdamT@users.noreply.github.com>
2024-03-04 21:02:57 +0000

    Update OFexception.hpp

--- include/libpatchfinder/OFexception.hpp
+++ include/libpatchfinder/OFexception.hpp
@@ -32,6 +32,7 @@ namespace tihmstar {
     
     class load_command_not_found : public OFexception {
         int _cmd;
+
         public:
             int cmd() const { return _cmd; };
 
@@ -39,6 +40,7 @@ namespace tihmstar {
             load_command_not_found(const char *commit_count_str, const char *commit_sha_str, int line, const char *filename, int cmd)
                 : OFexception(commit_count_str, commit_sha_str, line, filename, "failed to find cmd"), _cmd(cmd) {}
     };
+
     
     class symtab_not_found : public OFexception{
         using OFexception::OFexception;
